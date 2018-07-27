309e925ac61837f78c2b37794d2165e941e9d0c2
Troy Nichols <troygnichols@gmail.com>
2018-07-27 19:41:56 -0400

    Test update`

--- src/App.js
+++ src/App.js
@@ -20,6 +20,9 @@ import 'react-toastify/dist/ReactToastify.css';
 class App extends Component {
 
   render() {
+    // XXX temp
+    console.log('deleteme');
+
     return (
       <BrowserRouter className="App">
         <div className="App">
