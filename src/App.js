35f7b125e84b1640ef51d470369d01550a8724de
Troy Nichols <troygnichols@gmail.com>
2018-08-05 12:15:00 -0400

    Make footer stick to bottom

--- src/App.js
+++ src/App.js
@@ -50,57 +50,60 @@ class App extends Component {
     const { errorMessage } = this.state;
     return (
       <ErrorProvider value={{error: this.error}}>
-        <ModalError if={errorMessage}>
-          <i className="delete is-pulled-right"
-            onClick={this.dismissError}></i>
-          <p className="has-text-centered">
-            {errorMessage}
-          </p>
-          <p className="has-text-centered">
-            <a className="button"
-              onClick={this.dismissError}>Dismiss</a>
-          </p>
-        </ModalError>
-        <BrowserRouter className="App">
-          <div className="App">
-            <NavBar/>
+        <div id="wrap">
+          <ModalError if={errorMessage}>
+            <i className="delete is-pulled-right"
+              onClick={this.dismissError}></i>
+            <p className="has-text-centered">
+              {errorMessage}
+            </p>
+            <p className="has-text-centered">
+              <a className="button"
+                onClick={this.dismissError}>Dismiss</a>
+            </p>
+          </ModalError>
+          <BrowserRouter className="App">
+            <div className="App">
+              <NavBar/>
 
-            <div className="section">
-              <div className="container">
-                <ToastContainer position={toast.POSITION.TOP_RIGHT}
-                  className="toast-message"
-                  hideProgressBar={true}
-                  draggable
-                  autoClose={5000}
-                  newestOnTop={false}
-                  closeOnClick={false}
-                  pauseOnVisibilityChange={false}
-                  transition={Zoom} />
-                <Switch>
-                  <Redirect exact from='/' to='/invoices' />
-                  <Route exact path='/login' component={Login} />
-                  <Route exact path='/signup' component={Signup} />
-                  <Route exact path='/signup/success' component={SignupSuccess} />
-                  <Route exact path='/profile' component={Profile} />
-                  <Route exact path='/change_password' component={ChangePassword} />
-                  <Route exact path='/invoices' component={InvoiceList} />
-                  <Route exact path='/invoices/new' component={NewInvoice} />
-                  <Route exact path='/invoices/:id' component={Invoice} />
-                  <Route exact path='/invoices/:id/edit' component={EditInvoice} />
-                </Switch>
+              <div className="section">
+                <div className="container">
+                  <ToastContainer position={toast.POSITION.TOP_RIGHT}
+                    className="toast-message"
+                    hideProgressBar={true}
+                    draggable
+                    autoClose={5000}
+                    newestOnTop={false}
+                    closeOnClick={false}
+                    pauseOnVisibilityChange={false}
+                    transition={Zoom} />
+                  <Switch>
+                    <Redirect exact from='/' to='/invoices' />
+                    <Route exact path='/login' component={Login} />
+                    <Route exact path='/signup' component={Signup} />
+                    <Route exact path='/signup/success' component={SignupSuccess} />
+                    <Route exact path='/profile' component={Profile} />
+                    <Route exact path='/change_password' component={ChangePassword} />
+                    <Route exact path='/invoices' component={InvoiceList} />
+                    <Route exact path='/invoices/new' component={NewInvoice} />
+                    <Route exact path='/invoices/:id' component={Invoice} />
+                    <Route exact path='/invoices/:id/edit' component={EditInvoice} />
+                  </Switch>
+                </div>
               </div>
             </div>
+          </BrowserRouter>
+        </div>
 
-            <footer className="footer">
-              <div className="container has-text-centered">
-                <p>
-                  The source code for this project is <strong><a href="https://github.com/troygnichols/zark-billing-react"
-                      target="_blank" rel="noopener noreferrer">here</a></strong>.
-                </p>
-              </div>
-            </footer>
+        <footer className="footer">
+          <div className="container has-text-centered">
+            <p>
+              The source code for this project is <strong><a href="https://github.com/troygnichols/zark-billing-react"
+                  target="_blank" rel="noopener noreferrer">here</a></strong>.
+            </p>
           </div>
-        </BrowserRouter>
+        </footer>
+
       </ErrorProvider>
     );
   }
